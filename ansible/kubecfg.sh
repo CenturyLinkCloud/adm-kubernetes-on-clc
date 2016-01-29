a41f7c7ca329260559ca6bb9a9621d8867b11309
ntfrnzn <nathan.franzen@ctl.io>
2016-01-29 10:17:33 -0800

    add create-config script, minor typos

---
+++ ansible/kubecfg.sh
@@ -0,0 +1,38 @@
+export CLC_CLUSTER_NAME=k8s_2016-01-28T10-58-17-0800
+
+export k8s_cluster=thursday
+export k8s_user=admin
+export k8s_ns=default
+
+export master_ip=10.141.117.15
+
+# set default kube config file location to local file kubecfg_${k8s_cluster}
+OLDKUBECONFIG=${KUBECONFIG-~/.kube/config}
+KUBECONFIG="$(pwd)/kubecfg_${k8s_cluster}"
+
+# set cluster
+kubectl config set-cluster ${k8s_cluster} \
+   --server https://${master_ip}:6443 \
+   --insecure-skip-tls-verify=false \
+   --embed-certs=true \
+   --certificate-authority=${CLC_CLUSTER_NAME}.d/k8s_certs/ca.crt
+
+# user, credentials (reusing the kubelet/kube-proxy certificate)
+kubectl config set-credentials ${k8s_user}/${k8s_cluster} \
+   --embed-certs=true \
+   --client-certificate=${CLC_CLUSTER_NAME}.d/k8s_certs/kubecfg.crt \
+   --client-key=${CLC_CLUSTER_NAME}.d/k8s_certs/kubecfg.key
+
+# define context
+kubectl config set-context ${k8s_ns}/${k8s_cluster}/${k8s_user} \
+    --user=${k8s_user}/${k8s_cluster} \
+    --namespace=${k8s_ns} \
+    --cluster=${k8s_cluster} \
+
+# apply
+kubectl config use-context ${k8s_ns}/${k8s_cluster}/${k8s_user}
+
+# test
+kubectl cluster-info
+
+export KUBECONFIG="${KUBECONFIG}:${OLDKUBECONFIG}"
