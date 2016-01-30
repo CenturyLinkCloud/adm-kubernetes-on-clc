7755db8610763e8256647b582167008cb7630cb6
ntfrnzn <nathan.franzen@ctl.io>
2016-01-29 16:46:39 -0800

    .

--- ansible/kubecfg.sh
+++ ansible/kubecfg.sh
@@ -1,36 +1,40 @@
-export CLC_CLUSTER_NAME=k8s_2016-01-28T10-58-17-0800
 
-export k8s_cluster=thursday
-export k8s_user=admin
-export k8s_ns=default
+if [ -z ${CLC_CLUSTER_NAME+null_if_undefined}]
+  then echo please define environment variable CLC_CLUSTER_NAME
+  exit 1
+fi
 
-export master_ip=10.141.117.15
+export K8S_CLUSTER=${K8S_CLUSTER-$CLC_CLUSTER_NAME}
+export K8S_USER=${K8S_USER-admin}
+export K8S_NS=${K8S_NS-default}
 
-# set default kube config file location to local file kubecfg_${k8s_cluster}
+export master_ip=10.141.117.12
+
+# set default kube config file location to local file kubecfg_${K8S_CLUSTER}
 OLDKUBECONFIG=${KUBECONFIG-~/.kube/config}
-KUBECONFIG="$(pwd)/kubecfg_${k8s_cluster}"
+export KUBECONFIG="$(pwd)/kubecfg_${K8S_CLUSTER}"
 
 # set cluster
-kubectl config set-cluster ${k8s_cluster} \
+kubectl config set-cluster ${K8S_CLUSTER} \
    --server https://${master_ip}:6443 \
    --insecure-skip-tls-verify=false \
    --embed-certs=true \
    --certificate-authority=${CLC_CLUSTER_NAME}.d/k8s_certs/ca.crt
 
 # user, credentials (reusing the kubelet/kube-proxy certificate)
-kubectl config set-credentials ${k8s_user}/${k8s_cluster} \
+kubectl config set-credentials ${K8S_USER}/${K8S_CLUSTER} \
    --embed-certs=true \
    --client-certificate=${CLC_CLUSTER_NAME}.d/k8s_certs/kubecfg.crt \
    --client-key=${CLC_CLUSTER_NAME}.d/k8s_certs/kubecfg.key
 
 # define context
-kubectl config set-context ${k8s_ns}/${k8s_cluster}/${k8s_user} \
-    --user=${k8s_user}/${k8s_cluster} \
-    --namespace=${k8s_ns} \
-    --cluster=${k8s_cluster} \
+kubectl config set-context ${K8S_NS}/${K8S_CLUSTER}/${K8S_USER} \
+    --user=${K8S_USER}/${K8S_CLUSTER} \
+    --namespace=${K8S_NS} \
+    --cluster=${K8S_CLUSTER} \
 
 # apply
-kubectl config use-context ${k8s_ns}/${k8s_cluster}/${k8s_user}
+kubectl config use-context ${K8S_NS}/${K8S_CLUSTER}/${K8S_USER}
 
 # test
 kubectl cluster-info
