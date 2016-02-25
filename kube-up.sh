d6190f1331c5b2304e40011fae40eaa2d9bf751c
ntfrnzn <nathan.franzen@ctl.io>
2016-02-25 14:13:34 -0800

    make clc server names searchable with distinctive tag, change ansible group names, remove some ansible deprecation warnings

--- kube-up.sh
+++ kube-up.sh
@@ -53,6 +53,7 @@ function exit_message() {
 
 # default values before reading the command-line args
 datacenter=VA1
+etcd_group=kube-master
 minion_count=2
 minion_type=standard
 server_config_id=default
@@ -102,6 +103,7 @@ case $i in
     --etcd_separate_cluster*)
     # the ansible variable "etcd_group" has default value "master"
     etcd_separate_cluster=yes
+    etcd_group=etcd
     shift # past argument with no value
     ;;
 
@@ -158,7 +160,9 @@ else
 
   cat <<CONFIG > ${CLC_CLUSTER_HOME}/config/master_config.yml
 clc_cluster_name: ${CLC_CLUSTER_NAME}
-server_group: master
+server_group: kube-master
+etcd_group: ${etcd_group}
+server_group_tag: master
 datacenter: ${datacenter}
 server_count: 1
 server_config_id: default
@@ -171,7 +175,8 @@ CONFIG
 
   cat <<CONFIG > ${CLC_CLUSTER_HOME}/config/minion_config.yml
 clc_cluster_name: ${CLC_CLUSTER_NAME}
-server_group: minion
+server_group: kube-node
+server_group_tag: node
 datacenter: ${datacenter}
 server_count: ${minion_count}
 minion_type: ${minion_type}
@@ -248,7 +253,7 @@ CONFIG
   # write timestamp into flag file
   date +%Y-%m-%dT%H-%M-%S%z > $created_flag
 
-fi # checking [ -e $hosts_file ]
+fi # checking [ -e $created_flag ]
 
 #### verify access
 ansible -i ${CLC_CLUSTER_HOME}/hosts -m shell -a uptime all
